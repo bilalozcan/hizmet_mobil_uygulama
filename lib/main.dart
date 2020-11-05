3dc29a14e7252ee3587c38e755930e5ef1e3f1a1
mehmetarsay <mehmetarsay5@gmail.com>
2020-11-05 15:17:08 +0300

    main dart düzenlendi

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    //return yapar
+    //return yaparr
     return Container();
   }
 }
