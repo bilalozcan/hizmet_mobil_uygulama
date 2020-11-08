a5b26dba540cd785dfca2ef8517d2b40ba705a11
FatihDursunUzer <fatihdursunuzer@gmail.com>
2020-11-08 05:26:38 +0300

    -Kullanıcı giriş yapmaya çalıştığında e postasını hatalı girdiğinde kullanıcıya bir hata mesajı gösterilmesi ile alakalı ufak bir düzenleme yapıldı.
    Uyarı,Onay,Bildirim gibi mesajları gösteren, ToastMessage.dart dosyasında bulunan showToast fonksiyonuna Color objesi alan yeni bir parametre eklendi.
    Bu yeni parametre sayesinde mesajlar türlerine göre farklı renklerde gösterilebilecek.

--- lib/utils/ToastMessage.dart
+++ lib/utils/ToastMessage.dart
@@ -9,11 +9,11 @@ toastMessage(String message)async
  return await Fluttertoast.showToast(msg: message,toastLength: Toast.LENGTH_LONG,backgroundColor: Colors.red,textColor: Colors.white,fontSize: 16);
 }
 
-void showToast(BuildContext context,String message) {
+void showToast(BuildContext context,String message,Color color) {
  final scaffoldMessenger = ScaffoldMessenger.of(context);
  scaffoldMessenger.showSnackBar(
   SnackBar(
-   content: Text(message),
+   content: Text(message,style: TextStyle(color:color),),
    action: SnackBarAction(
        label: 'TAMAM', onPressed: scaffoldMessenger.hideCurrentSnackBar),
   ),
