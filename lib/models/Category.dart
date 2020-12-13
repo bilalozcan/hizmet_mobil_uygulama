3abc707c719b1c0a1a1ad70367e2b8532acd1d3d
bilalozcan <bilalozcan015@gmail.com>
2020-12-13 20:21:43 +0300

    -Kategori Json Parser Tamamlandı -kategori Model Sınıf Tamamlandı

--- lib/models/Category.dart
+++ lib/models/Category.dart
@@ -1,8 +1,52 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+
 class Category {
-  String category;
-  List<SubCategory> subCategoryList;
+  LinkedHashMap<String, dynamic> categories;
+  List<String> categoryList;
+
+  Category({@required this.categories}) {
+    categoryList = List<String>();
+    for (String key in categories.keys) categoryList.add(key);
+  }
+
+  factory Category.fromJson(LinkedHashMap<String, dynamic> parsedJson) {
+    return Category(categories: parsedJson);
+  }
+
+  SubCategory getSubCategory(String key) {
+    return SubCategory.fromJson(categories[key]);
+  }
 }
 
 class SubCategory {
-  Map<String, dynamic> dataMap;
+  LinkedHashMap<String, dynamic> subCategories;
+  List<String> subCategoryList;
+
+  SubCategory({@required this.subCategories}) {
+    subCategoryList = List<String>();
+    for (String key in subCategories.keys) subCategoryList.add(key);
+  }
+
+  factory SubCategory.fromJson(LinkedHashMap<String, dynamic> parsedJson) {
+    return SubCategory(subCategories: parsedJson);
+  }
+
+  Data getData(String key) {
+    return Data(datas: subCategories[key]);
+  }
+}
+
+class Data {
+  List<dynamic> datas;
+
+  Data({@required this.datas});
+
+  factory Data.fromJson(List<dynamic> parsedJson) {
+    return Data(datas: parsedJson);
+  }
+
+  List<String> getData() {
+    return datas;
+  }
 }
--- lib/ui/HizmetVerPage.dart
+++ lib/ui/HizmetVerPage.dart
@@ -1,4 +1,9 @@
+import 'dart:collection';
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hizmet_mobil_uygulama/models/Category.dart';
 
 class HizmetVerPage extends StatefulWidget {
   @override
@@ -8,15 +13,38 @@ class HizmetVerPage extends StatefulWidget {
 class _HizmetVerPageState extends State<HizmetVerPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("List"),
-        Text("List"),
-        Text("List"),
-        Text("List"),
-        Text("List"),
-        Text("List"),
-      ],
+    veriOku();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hizmet Ver page"),
+      ),
+      body: Column(
+        children: [
+          Text("List"),
+          Text("List"),
+          Text("List"),
+          Text("List"),
+          Text("List"),
+          Text("List"),
+        ],
+      ),
     );
+  }
+  veriOku() async{
+    var gelenJson = await DefaultAssetBundle.of(context).loadString("assets/Category.json");
+    //debugPrint(gelenJson);
+
+    LinkedHashMap<String, dynamic> map = json.decode(gelenJson.toString());
+    Category category = Category.fromJson(map);
+    for(var i in category.categoryList)
+      debugPrint("KeyList: " + i);
+    SubCategory subCategory = category.getSubCategory("Ders");
+    for(var i in category.categoryList){
+      debugPrint("Ders KeyList: " + i);
+    }
+    Data data = subCategory.getData("Diğer");
+    for(var i in data.datas){
+      debugPrint("Diğer List: " + i);
+    }
   }
 }
