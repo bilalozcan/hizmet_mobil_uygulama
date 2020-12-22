22e1e670c373c94ec750da401bacf7b01f278f1d
Bilal Özcan <bilalozcan015@gmail.com>
2020-12-23 01:09:32 +0300

    -readFilterHizmet setState hatası için birkaç değişiklik yapıldı

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -60,10 +60,10 @@ class _HomePageState extends State<HomePage> {
     try {
       await _hizmetModel.readFilterHizmet(
           category: category, subCategory: subCategory, hizmet: hizmet);
-      if(_hizmetModel.hizmetler != null){
+      if (_hizmetModel.hizmetler != null) {
         _hizmetler = _hizmetModel.hizmetler;
       }
-    } catch(e) {
+    } catch (e) {
       debugPrint("Debugg");
     }
     setState(() {
@@ -148,10 +148,9 @@ class _HomePageState extends State<HomePage> {
                           margin: EdgeInsets.all(5),
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: NetworkImage(
-                              "https://media-exp1.licdn.com/dms/image/C4E03AQHzHgb0LxczlQ/profile-displayphoto-shrink_800_800/0/1581495839216?e=1614211200&v=beta&t=AepGW-2sHNwfjNl66CA2-ZD2oaVCpijNy0UODvru3A8")
-                            ),
+                                fit: BoxFit.fill,
+                                image: NetworkImage(
+                                    "https://media-exp1.licdn.com/dms/image/C4E03AQHzHgb0LxczlQ/profile-displayphoto-shrink_800_800/0/1581495839216?e=1614211200&v=beta&t=AepGW-2sHNwfjNl66CA2-ZD2oaVCpijNy0UODvru3A8")),
                             color: Colors.blue,
                             borderRadius: new BorderRadius.all(
                                 new Radius.circular(
@@ -183,6 +182,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     UserModel _userModel = Provider.of<UserModel>(context);
+    final _hizmetModel = Provider.of<HizmetModel>(context, listen: false);
     return WillPopScope(
       onWillPop: () async {
         exit(0);
@@ -241,9 +241,18 @@ class _HomePageState extends State<HomePage> {
                                 },
                               ),
                             ),
+                            IconButton(
+                              icon: Icon(Icons.phone),
+                              onPressed: () {
+                                _hizmetModel.readFilterHizmet(
+                                    category: selectCategory,
+                                    subCategory: selectSubCategory,
+                                    hizmet: selectHizmet);
+                              },
+                            ),
                             Container(
                               padding: EdgeInsets.only(top: 5),
-                              width: MediaQuery.of(context).size.width / 1.2,
+                              width: MediaQuery.of(context).size.width / 2,
                               child: TextFormField(
                                 decoration: InputDecoration(
                                   border: OutlineInputBorder(
@@ -279,36 +288,37 @@ class _HomePageState extends State<HomePage> {
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         GestureDetector(
-                          child:HideContainer(selectCategory, 50),
-                          onTap: (){
+                          child: HideContainer(selectCategory, 50),
+                          onTap: () {
                             setState(() {
-                              if(selectCategory!=null&&selectSubCategory==null){
-                              selectCategory=null;
+                              if (selectCategory != null &&
+                                  selectSubCategory == null) {
+                                selectCategory = null;
                               }
                             });
                           },
                         ),
                         GestureDetector(
-                          child:HideContainer(selectSubCategory, 40),
-                          onTap: (){
+                          child: HideContainer(selectSubCategory, 40),
+                          onTap: () {
                             setState(() {
-                              if(selectSubCategory!=null&&selectHizmet==null){
-                                selectSubCategory=null;
+                              if (selectSubCategory != null &&
+                                  selectHizmet == null) {
+                                selectSubCategory = null;
                               }
                             });
                           },
                         ),
                         GestureDetector(
-                          child:HideContainer(selectHizmet, 30),
-                          onTap: (){
+                          child: HideContainer(selectHizmet, 30),
+                          onTap: () {
                             setState(() {
-                              if(selectHizmet!=null){
-                                selectHizmet=null;
+                              if (selectHizmet != null) {
+                                selectHizmet = null;
                               }
                             });
                           },
                         ),
-
                         Expanded(
                           child: Selected() != null ? Selected() : Container(),
                         )
@@ -317,22 +327,69 @@ class _HomePageState extends State<HomePage> {
                   )),
                   //newsListSliver,
                   SliverList(
-                      delegate: SliverChildBuilderDelegate((context, index) {
-                    return FutureBuilder(
-                      future: hizmetCard(_hizmetler[index]),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData)
-                          return snapshot.data;
-                        else
-                          return Center(
-                            child: CircularProgressIndicator(
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                      },
-                    );
-                    //else return CircularProgressIndicator(backgroundColor: Colors.red,);
-                  }, childCount: _hizmetler.length)),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        debugPrint("SliverChildBuilderDelegate 322.Satır");
+                        return Consumer<HizmetModel>(
+                          builder: (context, hizmetModel, child) {
+
+                            return Expanded(
+                              child:  Card(
+                                child: ListTile(
+                                  title: Text(hizmetModel
+                                      .hizmetler[0].title),
+                                  subtitle: Text(hizmetModel
+                                      .hizmetler[0].detail),
+                                ),
+                              ),
+                              /*ListView.builder(
+                                itemBuilder: (context, index2) {
+                                  if (hizmetModel.hizmetler.isEmpty)
+                                    return Container();
+                                  else
+                                    return Card(
+                                      child: ListTile(
+                                        title: Text(hizmetModel
+                                            .hizmetler[index2].title),
+                                        subtitle: Text(hizmetModel
+                                            .hizmetler[index2].detail),
+                                      ),
+                                    );
+                                },
+                                itemCount: hizmetModel.hizmetler.length,
+                              ),*/
+                            );
+                          },
+                        );
+                        /*FutureBuilder(
+                          future: _hizmetModel.readFilterHizmet(
+                              category: selectCategory,
+                              subCategory: selectSubCategory,
+                              hizmet: selectHizmet),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              debugPrint("if");
+                              Hizmet hizmet2 = snapshot.data[index];
+                              return Card(
+                                child: ListTile(
+                                  title: Text(hizmet2.title),
+                                  subtitle: Text(hizmet2.detail),
+                                ),
+                              );
+                            } else {
+                              debugPrint("else");
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                            }
+                          },
+                        );*/
+                        //else return CircularProgressIndicator(backgroundColor: Colors.red,);
+                      },childCount: 2,
+                    ),
+                  ),
                 ],
               );
             } else {
@@ -355,9 +412,7 @@ class _HomePageState extends State<HomePage> {
       visible: name != null ? true : false,
       child: Container(
         padding: EdgeInsets.only(top: 9, left: 3, right: 3),
-        child: Column(
-            children: [
-
+        child: Column(children: [
           Container(
             height: size,
             width: size,
@@ -391,19 +446,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   Selected() {
-    if (selectCategory == null){
+    if (selectCategory == null) {
       return categoryList(_category.categoryList, 50, setState: setState);
-    }
-
-    else if (selectSubCategory == null){
+    } else if (selectSubCategory == null) {
       return categoryList(_subcategoryList, 50, setState: setState);
-    }
-    else if (selectHizmet == null){
+    } else if (selectHizmet == null) {
       return categoryList(_hizmetList, 50, setState: setState);
-    }else{
-      readFilterHizmet(selectCategory, selectSubCategory, selectHizmet);
-
+    } else {
+      //readFilterHizmet(selectCategory, selectSubCategory, selectHizmet);
     }
-
   }
 }
